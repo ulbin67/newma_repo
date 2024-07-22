e8ac670cb74ff511abb3a508ead8dd47841cb250
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-07-22 21:22:31 +0900

    모델 정의
    정의

--- project_/manage_apply/urls.py
+++ project_/manage_apply/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("apply_box/",views.box_apply_call),
     path("create_apply/", views.box_apply_create, name='box_apply_create'),
     path("apply_check/", views.box_checkcall, name='apply_check'),
+    path("manage/", views.BoxApplyList.as_view())
 ]
