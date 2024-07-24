5344e5ecc13d748deaca19a9675a79d58745a808
ChoYoungwon <74918493+Youngwonzone1@users.noreply.github.com>
2024-07-24 16:46:04 +0900

    상자부분 유효성 검사

--- project_/manage_apply/admin.py
+++ project_/manage_apply/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import apply,get_num
 
 # Register your models here.
-admin.site.register(apply)
+# admin.site.register(apply)
 admin.site.register(get_num)
+
+@admin.register(apply)
+class ApplyAdmin(admin.ModelAdmin):
+    field_names = tuple([f.name for f in apply._meta.get_fields()])
+    list_display = field_names
+    list_filter = ('apply_at',)
