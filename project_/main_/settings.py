c9c57a10c0a56e6f067af9e0ecfde2019b23dbcb
ekruddl <154015184+ekruddl@users.noreply.github.com>
2024-07-25 03:16:19 +0900

    qna 수정
    qna 수정

--- project_/main_/settings.py
+++ project_/main_/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "main_.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- project_/qna/urls.py
+++ project_/qna/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("faq/", views.faq_list, name='faq_list'),
+    path("qna/", views.faq_list, name='faq_list'),
 ]
 
