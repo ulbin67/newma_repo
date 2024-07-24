443eb72c2cffea0bfbb8646693f09f6e19c71bdf
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-07-24 18:02:33 +0900

    혹시 몰라 form 만들기
    .

--- project_/manage_apply/forms.py
+++ project_/manage_apply/forms.py
@@ -1,20 +1,19 @@
 from django import forms
 from .models import apply
 from .validators import phone_number_validate
+from django.contrib.auth.forms import UserCreationForm
 
 class ApplyForm(forms.ModelForm):
-    company = forms.CharField(max_length=10, unique=True)
-    com_num = forms.CharField(max_length=11)
-
-    #주소
-    address_num = forms.CharField(max_length=5)
-    address_info = forms.TextField()
-    address_detail = forms.TextField()
-    deli_request = forms.TextField()
-    
-    #신청자 정보
-    applicant = forms.CharField(max_length=10)
-    apcan_phone = forms.CharField(max_length=11)
-
-    #박스 수
-    box_num = forms.IntegerField()
+    class Meta:
+        model = apply
+        fields = [
+            'company',
+            'com_num',
+            'applicant',
+            'apcan_phone',
+            'address_num',
+            'address_info',
+            'address_detail',
+            'deli_request',
+            'box_num',
+        ]
