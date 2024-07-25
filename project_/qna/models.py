54a3c76a4d3e56b97766100425d7c6ca5806e4f3
ekruddl <154015184+ekruddl@users.noreply.github.com>
2024-07-25 15:53:23 +0900

    qna 답변, 예시
    답변 페이지 추가, data 예시 추가

--- project_/qna/models.py
+++ project_/qna/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class FAQ(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    answer = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+    
+
--- project_/qna/urls.py
+++ project_/qna/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("qna/", views.faq_list, name='faq_list'),
+    path('faq/<int:pk>/', views.faq_detail, name='faq_detail'),
 ]
 
