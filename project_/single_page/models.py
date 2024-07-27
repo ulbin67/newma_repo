335d00795ce8b9fcc9bb65a2fbb27ab88e118f2d
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-07-27 18:31:40 +0900

    .
    .

--- project_/single_page/models.py
+++ project_/single_page/models.py
@@ -12,14 +12,12 @@ class User(AbstractUser):
 
     name = models.CharField(max_length=10, null=True)
 
-    # address_num = models.CharField(max_length=5,null=True,blank=True)
-    # address_info = models.TextField(null=True,blank=True)
-    # address_detail = models.TextField(null=True,blank=True)
-    # deli_request = models.TextField(null=True,blank=True)
+    address_num = models.CharField(max_length=5,null=True,blank=True)
+    address_info = models.TextField(null=True,blank=True)
+    address_detail = models.TextField(null=True,blank=True)
+    deli_request = models.TextField(null=True,blank=True)
     
    
     phone_num = models.CharField(max_length=14,null=True,blank=False,unique=True)
-
-    company = models.ForeignKey(CompanyInfo,null=True)
 
 
