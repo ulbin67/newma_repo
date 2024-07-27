e7ec13fa44cd87b69d8929b6c797a85199fe91a2
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-07-27 17:27:27 +0900

    유저 모델 수정
    모델 수정

--- project_/single_page/models.py
+++ project_/single_page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from manage_apply.models import CompanyInfo
 
 # Create your models here.\
 
@@ -11,12 +12,14 @@ class User(AbstractUser):
 
     name = models.CharField(max_length=10, null=True)
 
-    recent_address_num = models.CharField(max_length=5,null=True,blank=True)
-    recent_address_info = models.TextField(null=True,blank=True)
-    recent_address_detail = models.TextField(null=True,blank=True)
-    recent_deli_request = models.TextField(null=True,blank=True)
+    # address_num = models.CharField(max_length=5,null=True,blank=True)
+    # address_info = models.TextField(null=True,blank=True)
+    # address_detail = models.TextField(null=True,blank=True)
+    # deli_request = models.TextField(null=True,blank=True)
     
-    recent_company = models.CharField(max_length=10,null=True,blank=False)
-    recent_num = models.CharField(max_length=14,null=True,blank=False,unique=True)
+   
+    phone_num = models.CharField(max_length=14,null=True,blank=False,unique=True)
+
+    company = models.ForeignKey(CompanyInfo,null=True)
 
 
