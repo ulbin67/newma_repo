b34ff247de3012f73a0cc3e6815a2429063a9a8b
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-07-27 18:46:40 +0900

    오류 수정
    .

--- project_/single_page/models.py
+++ project_/single_page/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from manage_apply.models import CompanyInfo
 
 # Create your models here.\
 
 #기본 유저 정의
 class User(AbstractUser):
-    # id = models.AutoField(primary_key=True, db_column='user_id')
-    username = models.CharField(primary_key=True, max_length=20, unique=True) #이게 아이디
+    id = models.AutoField(primary_key=True, db_column='user_id')
+    username = models.CharField(max_length=20, unique=True) #이게 아이디
     password = models.CharField(max_length=300, null=False)
 
     name = models.CharField(max_length=10, null=True)
@@ -16,8 +15,7 @@ class User(AbstractUser):
     address_info = models.TextField(null=True,blank=True)
     address_detail = models.TextField(null=True,blank=True)
     deli_request = models.TextField(null=True,blank=True)
-    
    
-    phone_num = models.CharField(max_length=14,null=True,blank=False,unique=True)
+    phone_num = models.CharField(max_length=14,null=True,blank=False)
 
 
