5f6e2605a3ecb74a3d61b1c65260afe29beb9458
ChoYoungwon <74918493+Youngwonzone1@users.noreply.github.com>
2024-07-31 15:38:39 +0900

    -

--- project_/qna/urls.py
+++ project_/qna/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("qna/", views.faq_list, name='faq_list'),
+    path("", views.faq_list, name='faq_list'),
     path('faq/<int:pk>/', views.faq_detail, name='faq_detail'),
 ]
 
