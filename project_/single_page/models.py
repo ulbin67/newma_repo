07f4367b2f9bd717846d262b3e4c8a6a5dea195e
ekruddl <154015184+ekruddl@users.noreply.github.com>
2024-08-03 16:27:36 +0900

    applypage
    디자인 수정

--- project_/single_page/models.py
+++ project_/single_page/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+<<<<<<< Updated upstream
+=======
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.\
+
+#기본 유저 정의
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True, db_column='user_id')
+    username = models.CharField(max_length=20, unique=True)                 # 아이디
+    password = models.CharField(max_length=300, null=False)
+    name = models.CharField(max_length=10, null=True)
+    company_name = models.CharField(max_length=10,null=True,blank=True)
+    address_num = models.CharField(max_length=5,null=True,blank=True)
+    address_info = models.TextField(null=True,blank=True)
+    address_detail = models.TextField(null=True,blank=True)
+    deli_request = models.TextField(null=True,blank=True)
+    phone_num = models.CharField(max_length=14,null=True,blank=False)
+>>>>>>> Stashed changes
 
 # Create your models here.
