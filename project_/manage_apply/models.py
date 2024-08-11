2ab94217c60d5f1d3bf740d72bd43c59cbc42bae
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-11 20:54:01 +0900

    .
    .

--- project_/manage_apply/models.py
+++ project_/manage_apply/models.py
@@ -84,8 +84,8 @@ class CompanyInfo(models.Model):
     recent_apply = models.DateTimeField(auto_now = True,null=True)
     recent_employee = models.CharField(max_length=10, null=True, blank=False)
 
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.company
