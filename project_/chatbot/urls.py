809b117b285ba6cd4b767a5d2f720bdf4cdf0bcc
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-11 16:07:13 +0900

    .
    .

--- project_/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-# myapp/urls.py
-
-from django.urls import path
-from .views import chatbot_response
-
-urlpatterns = [
-    # path('chatbot/', chatbot_response, name='chatbot_response'),
-    # 다른 URL 패턴들...
-]
