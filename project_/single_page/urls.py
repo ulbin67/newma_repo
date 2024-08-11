e85699272798ad8e3574296a68ebc24d61d27e8f
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-11 22:51:59 +0900

    .
    .

--- project_/single_page/urls.py
+++ project_/single_page/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.maincall, name='home'),
+    path("introdue_newma/", views.introcall),
     path("information_newma/",views.infocall),
 
     path("accounts/", include('django.contrib.auth.urls')),
