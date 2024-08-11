5260b5518b97461796ef76e67d1e3310a3c52962
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-11 22:52:11 +0900

    .
    .

--- project_/single_page/views.py
+++ project_/single_page/views.py
@@ -20,6 +20,11 @@ from django.contrib.auth import logout
 def maincall(request):
     return render(request, 'single_page/main.html')
 
+def introcall(request):
+    return render(
+        request,
+        'single_page/introduce.html'
+    )
 
 def infocall(request):
     return render(
